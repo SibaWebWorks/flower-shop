a94b4ef759efa7b835199a1c05e680d1ec72eec9
SibaWebWorks <sibabalwe.sinyaniso.king@gmail.com>
2026-01-20 21:42:06 +0000

    ENH: Button hierarchy, active nav state, cleaner section rhythm

--- src/js/header.js
+++ src/js/header.js
@@ -15,8 +15,37 @@ function updateCartBadge() {
   el.style.display = "inline-flex";
 }
 
+function setActiveNav() {
+  const currentFile =
+    window.location.pathname.split("/").pop() || "index.html";
+
+  document.querySelectorAll("nav.nav a").forEach((a) => {
+    a.classList.remove("active");
+
+    const rawHref = a.getAttribute("href") || "";
+    if (!rawHref) return;
+
+    // If link is an in-page anchor on index.html
+    if (rawHref.startsWith("./index.html#") || rawHref.startsWith("index.html#")) {
+      if (currentFile === "index.html") a.classList.add("active");
+      return;
+    }
+
+    // Compare file names (strip ./ and query/hash)
+    const hrefFile = rawHref
+      .replace(/^\.\//, "")
+      .split("?")[0]
+      .split("#")[0];
+
+    if (hrefFile === currentFile) {
+      a.classList.add("active");
+    }
+  });
+}
+
 // Initial
 updateCartBadge();
+setActiveNav();
 
 // Keep in sync if cart updates in another tab
 window.addEventListener("storage", (e) => {
